10950ccfaf2593aaf63718cb7f41e4cc612bd3b9
Dilshad Nirmal <dilshadgov2020@gmail.com>
2024-04-24 14:53:07 +0530

    hero page creation

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			container: {
+				center: "true",
+				padding: "1rem",
+			},
+			aria: {
+				current: 'current="page"',
+			},
+			fontFamily: {
+				sans: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				bkg: "#fff",
+				muted: "#F6EBDA",
+				accent1: "#FFBD7F",
+				accent2: "#A0583C",
+				dark2: "#835E4C",
+				dark1: "#2F2105",
+				text: "#000",
+			}
+		},
+	},
+	plugins: [],
+}
